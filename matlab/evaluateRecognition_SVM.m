d52d6ee24f1e4ba3e69769e73cd704005afcce93
Jimmy Jin <pengjuj@andrew.cmu.edu>
2015-03-02 01:37:20 -0500

    svmtry

--- matlab/evaluateRecognition_SVM.m
+++ matlab/evaluateRecognition_SVM.m
@@ -1,11 +1,22 @@
 load('vision.mat');
 load('dataset.mat');
 source = '../images/';
+numLabels = 8;
+model = cell(numLabels, 1);
+for k = 1:numLabels
+    model{k} = svmtrain(double(trainLabels==k),trainFeatures,'-b 1 -t 0 -q');
+end
+test_features=[];
+for i =1:testPaths
+    img_path = [source, trainPaths{i}];
+    img = imread(img_path);
+    wordMap = getVisualWords(img, dictionary);
+    img_hist = getImageFeatures(wordMap, size(dictionary, 1));
+    test_features = [test_features; img_hist];
+end
 
-model = svmtrain(trainFeatures,trainLabels,'-c 1');
-img_path = [source, testPaths{1}];
-img = imread(img_path);
-wordMap = getVisualWords(img, dictionary);
-img_hist = getImageFeatures(wordMap, size(dictionary, 1));
-label = svmpredict([1], img_hist, model);
-disp(label);
+prob = zeros(200, numLabels);
+for k=1:numLabels
+    [predict,accur,p]=svmpredict(double(testLabels==k), test_featurse, model{k}, '-b 1');
+%     prob(:,k)=p(:,model{k}.Label==1);
+end
